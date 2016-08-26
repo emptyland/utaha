7932c0b65ff951a755ddc36d2ba69cb9ab71181f
emptyland <cjvoid@gmail.com>
2016-08-26 17:37:21 +0800

    Update cdep version.

--- src/main.cc
+++ src/main.cc
@@ -1,4 +1,8 @@
 #include "GLFW/glfw3.h"
+#include "freetype/ft2build.h"
+#include FT_FREETYPE_H
+#include FT_GLYPH_H
+#include <OpenGL/gl.h>
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
